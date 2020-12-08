549c8576c411ad5619042efcc50a2cf9432af75b
Johannes Lantz <lithvia@gmail.com>
2020-12-08 21:26:13 +0100

    Fixed keep alive

--- index.js
+++ index.js
@@ -59,19 +59,23 @@ client.on("message", message => {
         }
     });
 
+    //TODO - fix
     if(messageArray.includes("golv-help")) {
         //TODO - Add help message
     }
 
+    //TODO - fix
     if(messageArray.includes("tak")) {
          //TODO - Add roof message
     }
 
+    //TODO - fix
     if(messageArray.includes("golv")) {
         message.react("768160415150112797");
         message.channel.send("", {files: ["https://media1.tenor.com/images/2eb0cad8576bab400181013595e90bbd/tenor.gif?itemid=4756231"]});
     }
 
+    //TODO - fix
     if(messageArray.includes("<:Golvleggaren:768160415150112797>")) {
         message.react("768160415150112797");
         message.channel.send("Jag lägger golv!");
@@ -84,23 +88,7 @@ http.createServer(function (request, response) {}).listen(PORT);
 
 const startKeepAlive = () => {
     setInterval(function() {
-        var options = {
-            host: 'https://golvlaggaren.herokuapp.com/',
-            port: PORT,
-            path: '/'
-        };
-        http.get(options, function(res) {
-            res.on('data', function(chunk) {
-                try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
-                } catch (err) {
-                    console.log(err.message);
-                }
-            });
-        }).on('error', function(err) {
-            console.log("Error: " + err.message);
-        });
+        console.log("Keep alive");
     }, 20 * 60 * 1000); // load every 20 minutes
 }
 
