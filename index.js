967a16a064b52658371a3e50afc0ebdf322fe154
Johannes Lantz <lithvia@gmail.com>
2020-12-09 19:31:54 +0100

    Code refactoring

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const http = require("http");
 //Discord
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "!";
 
 //Commands
 const commands = {
@@ -19,19 +18,41 @@ const commands = {
 };
 
 //Nicknames
-//TODO - Add all user Ids
-const nickNames = {
-    "lantz": "<@242377088391315457>",
-    "hassel": "<@109946837850419200>",
-    "jeppe": "<@109939339575717888>",
-    "vera": "<@125277055066701824>",
-    "c": "<@>",
-    "lukas": "<@>",
-    "tim": "<@>",
-    "andre": "<@>"
-};
+const nicknames = [
+{
+    Name: ["lantz", "jojo"],
+    Id:  "<@242377088391315457>",
+},
+{
+  Name: ["hassel", "tönt"],
+  Id: "<@109946837850419200>"
+},
+{
+    Name: ["jeppe", "måsen"],
+    Id: "<@109939339575717888>"
+},
+{
+    Name: ["vera", "loremaster"],
+    Id:  "<@125277055066701824>"
+},
+{
+    Name: ["lukas", "volvopojken"],
+    Id:  "<@109442142849486848>"
+},
+{
+    Name: ["tim", "kuken"],
+    Id:  "<@110048899804184576>"
+},
+{
+    Name: ["c"],
+    Id:  "<@146693111358160898>"
+},
+{
+    Name: ["andre", "simp"],
+    Id:  "<@110494198699945984>"
+}];
 
-const floorTriggers = ["sämst", "dålig", "kass", "sist"];
+const floorTriggers = ["sämst", "dålig", "kass", "sist", "suger"];
 
 /*----- Bot begin -----*/
 
@@ -48,48 +69,36 @@ client.on("message", message => {
     const messageArray = message.content.toLowerCase().split(/ +/);
 
     //Check if message contains loser & tags user as a 
-    floorTriggers.forEach(word =>{
-        if(messageArray.includes(word)){
-            for(const key in nickNames){
-                if(messageArray.includes(key)){
-                    message.react("768160415150112797");
-                    return message.channel.send(`${nickNames[key]} lägger golv!`);
-                }
-            }
+    if(messageArray.some(word => floorTriggers.includes(word) && messageArray.length > 1)) {
+        const userToMention = nicknames.find(nickname => messageArray.find(msg => nickname.Name.includes(msg)));
+
+        if(userToMention.Name.includes("lantz")) {
+            return message.channel.send(`Bra försök <@${message.author.id}>! Lantz lägger tak!`);
+        } else if(userToMention !== "undefined") {
+            message.react("768160415150112797");
+            return message.channel.send(`${userToMention.Id} lägger golv!`);
         }
-    });
+    }
+
 
     //TODO - fix
-    if(messageArray.includes("golv-help")) {
+    if(messageArray.includes("!golv-help")) {
         //TODO - Add help message
     }
 
     //TODO - fix
-    if(messageArray.includes("tak")) {
+    if(messageArray.includes("!tak")) {
          //TODO - Add roof message
     }
 
     //TODO - fix
-    if(messageArray.includes("golv")) {
+    if(messageArray.includes("!golv")) {
         message.react("768160415150112797");
         message.channel.send("", {files: ["https://media1.tenor.com/images/2eb0cad8576bab400181013595e90bbd/tenor.gif?itemid=4756231"]});
     }
 
-    //TODO - fix
-    if(messageArray.includes("<:Golvleggaren:768160415150112797>")) {
+    if(messageArray.includes("<:golvleggaren:768160415150112797>")) {
         message.react("768160415150112797");
         message.channel.send("Jag lägger golv!");
     }
 });
-
-
-/* KEEP ALIVE */
-http.createServer(function (request, response) {}).listen(PORT);
-
-const startKeepAlive = () => {
-    setInterval(function() {
-        console.log("Keep alive");
-    }, 20 * 60 * 1000); // load every 20 minutes
-}
-
-startKeepAlive();
